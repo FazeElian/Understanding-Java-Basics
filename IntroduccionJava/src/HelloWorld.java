bd34a84412612775d011427fd3aeb6fa0f87dcbd
FazeElian <elian.ibarra@soy.sena.edu.co>
2026-02-14 19:10:26 -0500

    add: Java introduction module

---
+++ IntroduccionJava/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) { // Main method
+        System.out.println("Hello World"); // Print line on console
+        System.out.println("This is my first time using Java :)");
+    }
+}
---
+++ IntroduccionJava/src/IntroducingMyself.java
@@ -0,0 +1,6 @@
+public class IntroducingMyself {
+    public static void main(String[] args) {
+        System.out.println("My name is Elián Ibarra");
+        System.out.println("And i'm 17 years old");
+    }
+}
